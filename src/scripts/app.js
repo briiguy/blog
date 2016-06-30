99fe2e1c81d174592654cf0662973467c773e7d6
Brian Evasiuk <brianevasiuk@Brians-MacBook-Air-2.local>
2016-06-30 09:31:05 -0500

    maybe

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,80 +1,267 @@
-const React = require('react'),
-	ReactDOM = require('react-dom')
+import Backbone from 'backbone'
+import ReactDOM from 'react-dom'
+import React from 'react'
 
 const app = function() {
 
-	const HomeView = React.createClass({
+
+
+	var MultiListingViewController = React.createClass({
+        render: function(){
+
+            console.log(this.props)
+            return (
+                <div className="appContainer">
+                    <SearchBar listingData={this.props.searchItems}/>
+                    <CategoryBar />
+                    <MultiListingDisplay listingData={this.props.listingData}  />
+
+
+                </div>
+
+                )
+
+        }
+
+    })
+
+    var SingleViewController = React.createClass({
+        render: function(){
+                return(
+                    <div className="appContainer">
+                    <SearchBar listingData={this.props.searchItems} />
+                    <CategoryBar />
+                    <SingleListingSummary />
+                    </div>
+            )
+        }
+    })
+
+    var SearchBar = React.createClass({
+        _handleSearch : function(e){
+            var target = e.target
+            console.log(target)
+            e.preventDefault()
+            location.hash = '#search/' + target.querySelector('input').value
+
+            target.querySelector('input').value =''
+
+
+
+        },
+
 		render: function() {
 			return (
-				<div id="wrapper">
-					<Header />
-					<Content />
-					<Sticky />
-				</div>
-				)
-		}
-	})
+                <form id="searchBar" onSubmit={this._handleSearch}>
+                    <div>
+                        <a href="#home">
+                        <img src="http://www.knitnatural.com/uploads/1/6/1/9/16193668/7025640_orig.png" id="logo" />
+                        </a>
+                        <input type="text" placeholder="Search Etsy" />
+                    </div>
+                </form>
 
-	const Header = React.createClass({
-		render: function(){
-			return (
-				<div id="header">
-					<img id="hero-img" src="http://magentanova.github.io/html-intro-1/images/houston.jpg"/>
-					<img id="logo"src="http://magentanova.github.io/html-intro-1/images/ironyardlogo.png"/>
-				</div>
-				)
+                )
 		}
 	})
 
 
-	const Content = React.createClass({
-		render: function(){
-			return(
-				<div id="content">
-					<div id="leftCol">
-						<h3> The Iron Yard | Houston </h3>
-				        <p> Happenings and updates from The Iron Yard in Houston, TX </p>
-				        <hr/>
-				        <p> SEARCH </p>
-				        <input type="text" name="search" placeholder="Search Keywords"/>
-				    </div>
-				    <div id="rightCol">
-						<h2> September 22 Starts a New Class of The Iron Yard Houston Students </h2>
-				        <p> By Brian Dorton, Campus Director at The Iron Yard Houston </p>
-				        <img src="http://magentanova.github.io/html-intro-1/images/classroom.jpg"/>
-				        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas reiciendis natus architecto nostrum aperiam repudiandae possimus a delectus expedita atque commodi aliquam corrupti voluptas nemo voluptate vero minus, voluptatibus fugiat.</p>
-				    </div>
-				</div>
-				)
+var CategoryBar = React.createClass({
+        render: function() {
+            return (
 
-		}
-	})
+           
 
-	const Sticky = React.createClass({
-		render: function(){
-			return (
-				<div id="sticky-box">
-					<div id="sticky-title">
-					    <strong> Never miss a post!</strong> <hr/>
-					  </div>
-					<div id="twitter">
-						<div id="stick-logo">
-							<img src="http://magentanova.github.io/html-intro-1/images/ironyardlogo.png"/>
-						</div>
-						<div id="twitter-account">
-						<p><strong>tiyhouston</strong><br/><span id="sticky-name">The Iron Yard | Houston </span></p>
-						</div>
-						<div id="twitter-button">
-						<a href="https://twitter.com/TheIronYard" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @TheIronYard</a>
-						</div>
-					</div>
-				</div>
-				)
-			
-		}
-	})
 
-	ReactDOM.render(<HomeView/>,document.querySelector('.container'))
+
+
+                )
+        }
+    })
+
+
+
+var MultiListingDisplay = React.createClass({
+    _createSingleJSXEls: function(modelsArray){
+        var jsxArray = []
+        for(var i = 0; i < modelsArray.length; i++){
+
+            var theJSX = <SingleListingSummary listingModel={modelsArray[i]} />
+            jsxArray.push(theJSX)
+        }
+        return jsxArray
+
+
+    },
+
+    render: function() {
+console.log('render()',this.props.listingData)
+        var listingModels = this.props.listingData.models || [this.props.listingData]
+
+        return (
+            <div id="container">{this._createSingleJSXEls(listingModels)}</div>
+
+
+
+            )
+
+
+    }
+
+})
+
+var SingleListingSummary = React.createClass({
+    render: function(){
+        var htmlPic=null
+
+         if (this.props.listingModel.get('Images').length > 0){
+                    htmlPic = this.props.listingModel.get('Images')[0].url_170x135
+
+                }else{
+                    htmlPic='./placeholder.jpg'
+
+                }
+
+        return (<a
+                    href={"#details/"+this.props.listingModel.get('listing_id')}
+                    className="itemsContainer" data-pid={this.props.listingModel.get('listing_id')}>
+
+                 <p className="title">{this.props.listingModel.get('title')}</p>
+                 <img src={htmlPic} />
+                 <p classNAme="price">{ this.props.listingModel.get('currency_code') + '  ' + this.props.listingModel.get('price') }</p>
+                </a>
+
+
+
+            )
+
+
+    }
+
+
+})
+
+
+
+//keystring : ub1hcoexwc9g7jywvexn9g22
+//shared secret : 8o6fy1cv39
+//url : https://openapi.etsy.com/v2/listings/active.js?api_key=ub1hcoexwc9g7jywvexn9g22
+//
+
+
+
+var ItemsCollection = Backbone.Collection.extend({
+    url: 'https://openapi.etsy.com/v2/listings/active.js',
+//    '?includes=Images,Shop&',
+    _key: 'enmqsnumlj89wy25oyiuz7su',
+    parse: function(res) {
+
+        return res.results
+    }
+})
+
+
+var ItemModel = Backbone.Collection.extend({
+
+
+    generateUrl : function (id) {
+        this.url ='https://openapi.etsy.com/v2/listings/'+ id +'.js?includes=Images,Shop&api_key=aavnvygu0h5r52qes74x9zvo&callback=?'},
+    parse: function(res){
+        return res.results
 }
 
+})
+
+
+var EtsyRouter = Backbone.Router.extend({
+    routes: {
+        "details/:id": "doDetailView",
+        "search/:keyword": "doItemSearchView",
+        "home": "doListView",
+        "*catchall": "goHome"
+    },
+
+    doDetailView: function(id) {
+
+
+        var item = new ItemModel()
+        item.generateUrl(id)
+        console.log(item)
+        item.fetch({
+
+            dataType: 'jsonp',
+            data: {
+                api_key: item._key,
+                includes: 'Images,Shop',
+                processData: true,
+                }
+
+        }).then( function(d){
+                 ReactDOM.render(<MultiListingViewController  listingData={item} />,document.querySelector('.container'))
+        }
+
+        )
+    },
+
+
+    doItemSearchView: function(keyword) {
+        console.log(keyword)
+        var searchItems = new ItemsCollection(keyword)
+        searchItems.fetch({
+
+                dataType: 'jsonp',
+                data: {
+                    api_key: searchItems._key,
+                    includes: 'Images,Shop',
+                    keywords: keyword,
+                    limit:24,
+                    processData: true,
+                }
+
+        }).then( function(d){
+                 ReactDOM.render(<MultiListingViewController  listingData={searchItems} />,document.querySelector('.container'))
+        }
+
+        )
+
+
+
+
+    },
+
+    doListView: function() {
+    //    console.log('home view')
+        var listItems = new ItemsCollection()
+
+        listItems.fetch({
+
+            dataType: 'jsonp',
+            data: {
+                api_key: listItems._key,
+                includes: 'Images,Shop',
+                limit: 24,
+                processData: true,
+                }
+
+        }).then( function(d){
+            ReactDOM.render(<MultiListingViewController  listingData={listItems} />,document.querySelector('.container'))
+        }
+        )
+
+    },
+
+    goHome: function(){
+        location.hash = 'home'},
+
+    initialize: function() {
+
+        Backbone.history.start()
+
+    }
+})
+
+new EtsyRouter()
+
+
+}
 app()
